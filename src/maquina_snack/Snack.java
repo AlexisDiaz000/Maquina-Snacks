cb260cc8f6a87f3adf627cd4b4f6c7c4aa0ab7f0
alexis diaz <axel.608117@gmail.com>
2025-02-06 10:02:54 -0500

    finalizacion de la maquina de snack

--- src/maquina_snack/Snack.java
+++ src/maquina_snack/Snack.java
@@ -3,33 +3,28 @@ package maquina_snack;
 import java.io.Serializable;
 import java.util.Objects;
 
-// creacion de clase beans
 public class Snack implements Serializable {
-    //atributos.
-    private static int contadorSnack = 0;
+    private static int contadorSnacks = 0;
     private int idSnack;
     private String nombre;
     private double precio;
-    //constructor
+
     public Snack(){
-        this.idSnack = ++Snack.contadorSnack;
+        this.idSnack = ++Snack.contadorSnacks;
     }
+
     public Snack(String nombre, double precio){
-        this();//debe ser el primer linea la llamada al constructor.
+        this(); // Debe ser la primer linea la llamada al constructor
         this.nombre = nombre;
         this.precio = precio;
     }
-    //solo se genera el get de contadorSnack ya que el set no se utiliza
-    public static void setContadorSnack(int contadorSnack) {
-        Snack.contadorSnack = contadorSnack;
-    }
-    // se crean los getter y setter de los atributos de la clase
-    public int getIdSnack() {
-        return idSnack;
+
+    public static int getContadorSnacks() {
+        return contadorSnacks;
     }
 
-    public void setIdSnack(int idSnack) {
-        this.idSnack = idSnack;
+    public int getIdSnack() {
+        return idSnack;
     }
 
     public String getNombre() {
@@ -57,14 +52,15 @@ public class Snack implements Serializable {
                 '}';
     }
 
-    @Override  //equals para realizar las comparaciones
+    @Override
     public boolean equals(Object o) {
+        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Snack snack = (Snack) o;
         return idSnack == snack.idSnack && Double.compare(precio, snack.precio) == 0 && Objects.equals(nombre, snack.nombre);
     }
 
-    @Override //hashCode para hacer comparaciones de tipo numerico.
+    @Override
     public int hashCode() {
         return Objects.hash(idSnack, nombre, precio);
     }
